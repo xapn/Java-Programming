98f6a0515e3c9f17d42c5a88c20764aff3c89abe
Xavier Pigeon <xpn.projects@gmail.com>
2012-05-28 22:51:43 +0200

    Issue #14 - Wrapping a Maven POM with Ant.

---
+++ build-wrapping/maven-into-ant/maven-built-artifact/src/main/java/xapn/javapro/buildwrapping/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package xapn.javapro.buildwrapping;
+
+/**
+ * Hello World.
+ * 
+ * @author Xavier Pigeon
+ */
+public class HelloWorld {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ build-wrapping/maven-into-ant/maven-built-artifact/src/test/java/xapn/javapro/buildwrapping/HelloWorldTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package xapn.javapro.buildwrapping;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test case for {@link HelloWorld}.
+ * 
+ * @author Xavier Pigeon
+ */
+public class HelloWorldTest {
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {}
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {}
+    
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
